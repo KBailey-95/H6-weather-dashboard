2350d48f051e33c8d97c6b328556116105ac5d8d
Beast <95392136+KBailey-95@users.noreply.github.com>
2022-03-18 02:21:04 -0400

    Creation of variables that will be needed for later calculations as well as functions for several day highs within the app.

--- index.js
+++ index.js
@@ -75,5 +75,281 @@ var day3Desc = '';
 var day4Desc = '';
 var day5Desc = '';
 
+const apiKey = "52828bd95a263fd4260316440728f92b";
+var userInput = '';
+var city;
+var state;
+var countryCode = "us";
+var cityState = city + "," + state + "," + countryCode;
+var zipCode;
+
+var urlFromZIP;
+var urlFromCityState = "https://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&appid=" + apiKey;
+var currentURL;
+var forecastURL;
+
+var dataFromCityState;
+var dataFromZIP;
+var latitude;
+var longitude;
+
+var cityStateArr = [];
+var currentData;
+var cityName;
+var currentDate = moment().format('llll');
+var currentMain;
+var currentTemp;
+var currentWind;
+var currentHumidity;
+var currentUVindex;
+
+var forecastData;
+
+var forecastedHighTemps = [];
+
+var forecastDay1Date = new Date()
+var forecastDay2Date = new Date()
+var forecastDay3Date = new Date()
+var forecastDay4Date = new Date()
+var forecastDay5Date = new Date()
+
+var day1Temps = [];
+var day2Temps = [];
+var day3Temps = [];
+var day4Temps = [];
+var day5Temps = [];
+var day1HighTemp;
+var day2HighTemp;
+var day3HighTemp;
+var day4HighTemp;
+var day5HighTemp;
+
+function displayForecastedHighs() {
+  day1TempEl.textContent = "Temp: " + day1HighTemp + "℉";
+  day2TempEl.textContent = "Temp: " + day2HighTemp + "℉";
+  day3TempEl.textContent = "Temp: " + day3HighTemp + "℉";
+  day4TempEl.textContent = "Temp: " + day4HighTemp + "℉";
+  day5TempEl.textContent = "Temp: " + day5HighTemp + "℉";
+  
+  day1WindEl.textContent = "Wind: " + day1HighWind + "mph";
+  day2WindEl.textContent = "Wind: " + day2HighWind + "mph";
+  day3WindEl.textContent = "Wind: " + day3HighWind + "mph";
+  day4WindEl.textContent = "Wind: " + day4HighWind + "mph";
+  day5WindEl.textContent = "Wind: " + day5HighWind + "mph";
+  
+  day1HumidityEl.textContent = "Humidity: " + day1HighHum + "%rh";
+  day2HumidityEl.textContent = "Humidity: " + day2HighHum + "%rh";
+  day3HumidityEl.textContent = "Humidity: " + day3HighHum + "%rh";
+  day4HumidityEl.textContent = "Humidity: " + day4HighHum + "%rh";
+  day5HumidityEl.textContent = "Humidity: " + day5HighHum + "%rh";
+}
+
+function getForecastedHighs() {
+  day1DayEl.textContent = (moment().add(1, 'days').format("dddd"));
+  day2DayEl.textContent = (moment().add(2, 'days').format("dddd"));
+  day3DayEl.textContent = (moment().add(3, 'days').format("dddd"));
+  day4DayEl.textContent = (moment().add(4, 'days').format("dddd"));
+  day5DayEl.textContent = (moment().add(5, 'days').format("dddd"));
+  day1DateEl.textContent = (moment().add(1, 'days').format("MMM Do YY"));
+  day2DateEl.textContent = (moment().add(2, 'days').format("MMM Do YY"));
+  day3DateEl.textContent = (moment().add(3, 'days').format("MMM Do YY"));
+  day4DateEl.textContent = (moment().add(4, 'days').format("MMM Do YY"));
+  day5DateEl.textContent = (moment().add(5, 'days').format("MMM Do YY"));
+  
+  day1Temps = [
+    forecastData.list[0].main.temp,
+    forecastData.list[1].main.temp,
+    forecastData.list[2].main.temp,
+    forecastData.list[3].main.temp,
+    forecastData.list[4].main.temp,
+    forecastData.list[5].main.temp,
+    forecastData.list[6].main.temp,
+    forecastData.list[7].main.temp
+  ];
+  day1HighTemp = Math.max.apply(null, day1Temps);
+  
+  day2Temps = [
+    forecastData.list[8].main.temp,
+    forecastData.list[9].main.temp,
+    forecastData.list[10].main.temp,
+    forecastData.list[11].main.temp,
+    forecastData.list[12].main.temp,
+    forecastData.list[13].main.temp,
+    forecastData.list[14].main.temp,
+    forecastData.list[15].main.temp
+  ];
+  day2HighTemp = Math.max.apply(null, day2Temps);
+  
+  day3Temps = [
+    forecastData.list[16].main.temp,
+    forecastData.list[17].main.temp,
+    forecastData.list[18].main.temp,
+    forecastData.list[19].main.temp,
+    forecastData.list[20].main.temp,
+    forecastData.list[21].main.temp,
+    forecastData.list[22].main.temp,
+    forecastData.list[23].main.temp
+  ];
+  day3HighTemp = Math.max.apply(null, day3Temps);
+  
+  day4Temps = [
+    forecastData.list[24].main.temp,
+    forecastData.list[25].main.temp,
+    forecastData.list[26].main.temp,
+    forecastData.list[27].main.temp,
+    forecastData.list[28].main.temp,
+    forecastData.list[29].main.temp,
+    forecastData.list[30].main.temp,
+    forecastData.list[31].main.temp
+  ];
+  day4HighTemp = Math.max.apply(null, day4Temps);
+  
+  day5Temps = [
+    forecastData.list[32].main.temp,
+    forecastData.list[33].main.temp,
+    forecastData.list[34].main.temp,
+    forecastData.list[35].main.temp,
+    forecastData.list[36].main.temp,
+    forecastData.list[37].main.temp,
+    forecastData.list[38].main.temp,
+    forecastData.list[39].main.temp
+  ];
+  day5HighTemp = Math.max.apply(null, day5Temps);
+  
+  day1Winds = [
+    forecastData.list[0].wind.speed,
+    forecastData.list[1].wind.speed,
+    forecastData.list[2].wind.speed,
+    forecastData.list[3].wind.speed,
+    forecastData.list[4].wind.speed,
+    forecastData.list[5].wind.speed,
+    forecastData.list[6].wind.speed,
+    forecastData.list[7].wind.speed
+  ];
+  day1HighWind = Math.max.apply(null, day1Winds);
+  
+  day2Winds = [
+    forecastData.list[8].wind.speed,
+    forecastData.list[9].wind.speed,
+    forecastData.list[10].wind.speed,
+    forecastData.list[11].wind.speed,
+    forecastData.list[12].wind.speed,
+    forecastData.list[13].wind.speed,
+    forecastData.list[14].wind.speed,
+    forecastData.list[15].wind.speed
+  ];
+  day2HighWind = Math.max.apply(null, day2Winds);
+  
+  day3Winds = [
+    forecastData.list[16].wind.speed,
+    forecastData.list[17].wind.speed,
+    forecastData.list[18].wind.speed,
+    forecastData.list[19].wind.speed,
+    forecastData.list[20].wind.speed,
+    forecastData.list[21].wind.speed,
+    forecastData.list[22].wind.speed,
+    forecastData.list[23].wind.speed
+  ];
+  day3HighWind = Math.max.apply(null, day3Winds);
+  
+  day4Winds = [
+    forecastData.list[24].wind.speed,
+    forecastData.list[25].wind.speed,
+    forecastData.list[26].wind.speed,
+    forecastData.list[27].wind.speed,
+    forecastData.list[28].wind.speed,
+    forecastData.list[29].wind.speed,
+    forecastData.list[30].wind.speed,
+    forecastData.list[31].wind.speed
+  ];
+  day4HighWind = Math.max.apply(null, day4Winds);
+  
+  day5Winds = [
+    forecastData.list[32].wind.speed,
+    forecastData.list[33].wind.speed,
+    forecastData.list[34].wind.speed,
+    forecastData.list[35].wind.speed,
+    forecastData.list[36].wind.speed,
+    forecastData.list[37].wind.speed,
+    forecastData.list[38].wind.speed,
+    forecastData.list[39].wind.speed
+  ];
+  day5HighWind = Math.max.apply(null, day5Winds);
+  
+  day1Hums = [
+    forecastData.list[0].main.humidity,
+    forecastData.list[1].main.humidity,
+    forecastData.list[2].main.humidity,
+    forecastData.list[3].main.humidity,
+    forecastData.list[4].main.humidity,
+    forecastData.list[5].main.humidity,
+    forecastData.list[6].main.humidity,
+    forecastData.list[7].main.humidity
+  ];
+  day1HighHum = Math.max.apply(null, day1Hums);
+  
+  day2Hums = [
+    forecastData.list[8].main.humidity,
+    forecastData.list[9].main.humidity,
+    forecastData.list[10].main.humidity,
+    forecastData.list[11].main.humidity,
+    forecastData.list[12].main.humidity,
+    forecastData.list[13].main.humidity,
+    forecastData.list[14].main.humidity,
+    forecastData.list[15].main.humidity
+  ];
+  day2HighHum = Math.max.apply(null, day2Hums);
+  
+  day3Hums = [
+    forecastData.list[16].main.humidity,
+    forecastData.list[17].main.humidity,
+    forecastData.list[18].main.humidity,
+    forecastData.list[19].main.humidity,
+    forecastData.list[20].main.humidity,
+    forecastData.list[21].main.humidity,
+    forecastData.list[22].main.humidity,
+    forecastData.list[23].main.humidity
+  ];
+  day3HighHum = Math.max.apply(null, day3Hums);
+  
+  day4Hums = [
+    forecastData.list[24].main.humidity,
+    forecastData.list[25].main.humidity,
+    forecastData.list[26].main.humidity,
+    forecastData.list[27].main.humidity,
+    forecastData.list[28].main.humidity,
+    forecastData.list[29].main.humidity,
+    forecastData.list[30].main.humidity,
+    forecastData.list[31].main.humidity
+  ];
+  day4HighHum = Math.max.apply(null, day4Hums);
+  
+  day5Hums = [
+    forecastData.list[32].main.humidity,
+    forecastData.list[33].main.humidity,
+    forecastData.list[34].main.humidity,
+    forecastData.list[35].main.humidity,
+    forecastData.list[36].main.humidity,
+    forecastData.list[37].main.humidity,
+    forecastData.list[38].main.humidity,
+    forecastData.list[39].main.humidity
+  ];
+  day5HighHum = Math.max.apply(null, day5Hums);
+
+  day1Desc = forecastData.list[0].weather[0].description
+  day2Desc = forecastData.list[1].weather[0].description
+  day3Desc = forecastData.list[2].weather[0].description
+  day4Desc = forecastData.list[3].weather[0].description
+  day5Desc = forecastData.list[4].weather[0].description
+
+  console.log("day 1 desciption is: " + day1Desc);
+  console.log("day 2 desciption is: " + day2Desc);
+  console.log("day 3 desciption is: " + day3Desc);
+  console.log("day 4 desciption is: " + day4Desc);
+  console.log("day 5 desciption is: " + day5Desc);
+
+  displayForecastedHighs();
+  updateForecastImages();
+}
 
 
